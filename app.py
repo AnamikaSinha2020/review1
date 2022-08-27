dbbc8baf2b36dc7294c021d301a2b3ec6c7f49e3
Manish Sinha <manishkumarsinha@gmail.com>
2022-08-27 09:01:00 -0400

    first commit

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])  # route to display the home page
+@app.route('/', methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
+@app.route('/review', methods=['POST','GET']) # route to show the review comments in a web UI
 @cross_origin()
 def index():
     if request.method == 'POST':
